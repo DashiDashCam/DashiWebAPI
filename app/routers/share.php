1b5f164c81330f590d277bfe1509a1605fc08f5f
cahenk <cahenk95@gmail.com>
2018-02-21 10:19:22 -0600

    Download video using shared link

--- app/routers/share.php
+++ app/routers/share.php
@@ -7,6 +7,25 @@ require_once __DIR__ . '/../middleware/Auth.php';
 
 $app->get('/Share/{id}', function (Request $request, Response $response) use ($app) {
 
+    $stmt = $this->db->prepare("SELECT videoContent FROM Shares JOIN Videos ON Shares.videoID=Videos.id WHERE Shares.id=:id");
+
+    $stmt->bindValue(':id', base64_decode($args['id']), PDO::PARAM_LOB);
+
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+
+    if ($row) {
+        return $response->getBody()->write($row['videoContent']);
+    }
+    else {
+        return $response->withJson([
+            'code' => 1054,
+            'message' => 'Video Not Found',
+            'description' => 'The provided video id is either invalid or you lack sufficient authorization'
+        ], 404);
+    }
+
 })->setName('downloadSharedVideo');
 
 $app->post('/Share', function (Request $request, Response $response) use ($app) {
